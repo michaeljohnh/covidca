9f6a126a9588d786894a80a90d1f3d7ca78d267a
michaeljohnh <michaelhilomen@gmail.com>
2020-03-29 16:40:50 -0700

    routing fixed

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var path = require('path');
 
 const app = express();
 const router = express.Router();
+const apiRouter = require('./routes/routes');
 
 const dbURI = 'mongodb+srv://michaelh:P6LqVssXCO8dMT9M@cluster0-b1ubn.mongodb.net/COVIDCA?retryWrites=true&w=majority';
 
@@ -34,97 +35,12 @@ connection.once('open', () => {
 require ('./models/locations');
 const Loc = mongoose.model('Location');
 
-router
-    .route('/api/locations')
-    .get(  async (req,res) => {
-
-      const lng = parseFloat(req.query.lng);
-      // const lng = -118.603219
-
-      const lat = parseFloat(req.query.lat);
-      //const lat = 34.228716;
-      //const max = 2000000;
-
-      const max = parseFloat(req.query.max);
-
-      if (!lat || !lng || !max) {
-        return res
-                .status(404)
-                .json({"message" : "need to provide lat lng max"});
-      }
-
-      console.log(lat);
-      console.log(lng);
-      const near = {
-        type: "Point",
-        coordinates: [lng, lat]
-      };
-      const geoOptions = {
-        distanceField: "distance.calculated",
-        spherical: true,
-        maxDistance: max
-      };
-
-      try {
-        const results =  await Loc.aggregate([
-
-          {
-            $geoNear: {
-              near,
-              ...geoOptions
-            },
-          },
-          { $match: {LICENSE_CATEGORY_DESC:"General Acute Care Hospital"} },
-
-          { $limit: 10 }
-        ]);
-        const locations = results.map(result => {
-          return {
-            id: result._id,
-            name: result.FACILITY_NAME,
-            street: result.streetAddress,
-            city: result.city,
-            zip: result.zipCode,
-            totalBeds : result.TOTAL_NUMBER_BEDS,
-            distance: `${result.distance.calculated.toFixed()}`
-          }
-        });
-
-        // console.log(results);
-
-        return res
-                .status(200)
-                .json(results);
-      } catch(err) {
-        console.log(err);
-      }
-
-
-
-    });
-
- router.route('/api/locations/:id')
-      .get( (req,res) => {  
-        Loc
-          .findById(req.params.id)
-          .exec((err, location) => {
-            if (!location) {
-              return res.status(404)
-                        .json({"message": "location not found"});
-            } else if (err) {
-              return res.status(404)
-                        .json(err);
-            }
-
-            console.log(location);
-            res
-              .status(200)
-              .json(location);
-          })
-        });
+app.use('/api',apiRouter);
 
 // app.use('/',router);
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+
 /**
  * Get port from environment and store in Express.
  */
---
+++ routes/routes.js
@@ -0,0 +1,98 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+require ('../models/locations');
+
+const Loc = mongoose.model('Location');
+
+router
+    .route('/locations')
+    .get(  async (req,res) => {
+
+      const lng = parseFloat(req.query.lng);
+      // const lng = -118.603219
+
+      const lat = parseFloat(req.query.lat);
+      //const lat = 34.228716;
+      //const max = 2000000;
+
+      const max = parseFloat(req.query.max);
+
+      if (!lat || !lng || !max) {
+        return res
+                .status(404)
+                .json({"message" : "need to provide lat lng max"});
+      }
+
+      console.log(lat);
+      console.log(lng);
+      const near = {
+        type: "Point",
+        coordinates: [lng, lat]
+      };
+      const geoOptions = {
+        distanceField: "distance.calculated",
+        spherical: true,
+        maxDistance: max
+      };
+
+      try {
+        const results =  await Loc.aggregate([
+
+          {
+            $geoNear: {
+              near,
+              ...geoOptions
+            },
+          },
+          { $match: {LICENSE_CATEGORY_DESC:"General Acute Care Hospital"} },
+
+          { $limit: 10 }
+        ]);
+        const locations = results.map(result => {
+          return {
+            id: result._id,
+            name: result.FACILITY_NAME,
+            street: result.streetAddress,
+            city: result.city,
+            zip: result.zipCode,
+            totalBeds : result.TOTAL_NUMBER_BEDS,
+            distance: `${result.distance.calculated.toFixed()}`
+          }
+        });
+
+        // console.log(results);
+
+        return res
+                .status(200)
+                .json(results);
+      } catch(err) {
+        console.log(err);
+      }
+
+
+
+    });
+
+ router.route('/locations/:id')
+      .get( (req,res) => {  
+        Loc
+          .findById(req.params.id)
+          .exec((err, location) => {
+            if (!location) {
+              return res.status(404)
+                        .json({"message": "location not found"});
+            } else if (err) {
+              return res.status(404)
+                        .json(err);
+            }
+
+            console.log(location);
+            res
+              .status(200)
+              .json(location);
+          })
+        });
+
+module.exports = router;
