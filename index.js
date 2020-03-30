27473239500481c6a9cc5b97fffd27c5784f484e
michaeljohnh <michaelhilomen@gmail.com>
2020-03-29 18:35:29 -0700

    typo fixes

--- index.js
+++ index.js
@@ -40,6 +40,9 @@ app.use('/api',apiRouter);
 // app.use('/',router);
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
+app.get('*', function(req, res, next) {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 /**
  * Get port from environment and store in Express.
